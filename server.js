dffbac7b800a434df83a2edf05d1977e0b05b019
Ikul23 <ikul23@icloud.com>
2025-02-19 19:38:17 +0300

    New order commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Сервер работает!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
