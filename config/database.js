72e9b1c94fe1872e7efecc050b313a41e7ef36f5
Ikul23 <ikul23@icloud.com>
2025-03-10 20:20:40 +0300

    Database conntction options

--- config/database.js
+++ config/database.js
@@ -1,21 +1,14 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
-    dialectOptions: isProduction
-      ? { ssl: { require: true, rejectUnauthorized: false } } 
-      : {},
-  }
-);
+    dialectOptions: process.env.NODE_ENV === "production"
+        ? { ssl: { require: true, rejectUnauthorized: false } } 
+        : {}
+});
 
 module.exports = sequelize;
