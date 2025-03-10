bc09fba03cfe8658cb68e3c5fabf4726c5736a5f
Ikul23 <ikul23@icloud.com>
2025-03-10 15:26:48 +0300

    New databases connection

--- config/database.js
+++ config/database.js
@@ -1,11 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
-    logging: false, // Отключает логи SQL-запросов
+    logging: false,
+    dialectOptions: isProduction
+        ? { ssl: { require: true, rejectUnauthorized: false } } 
+        : {} 
 });
 
 module.exports = sequelize;
