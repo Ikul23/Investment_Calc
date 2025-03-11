f64a38e0f60aba126e262b2c0b799c801fc242da
Ikul23 <ikul23@icloud.com>
2025-03-11 20:40:10 +0300

    Models succefuly added

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Project = require("./project");
+const CashFlow = require("./cashFlows");
+const FinancialResult = require("./financialResults");
+
+module.exports = { Project, CashFlow, FinancialResult };
