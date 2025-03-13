4be9ce5ccebb77d3923ee2b4bdde2f012b99f52f
Ikul23 <ikul23@icloud.com>
2025-03-13 16:11:26 +0300

    DB config migration

--- config/database.js
+++ config/database.js
@@ -1,28 +1,43 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+const { URL } = require("url");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-console.log("DB_PORT:", process.env.DB_PORT); // Для отладки, должен быть 5432
-
-const sequelize = isProduction
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
+let productionConfig = {};
+if (process.env.DATABASE_URL) {
+  const dbUrl = new URL(process.env.DATABASE_URL);
+  productionConfig = {
+    username: dbUrl.username,
+    password: dbUrl.password,
+    database: dbUrl.pathname.slice(1),
+    host: dbUrl.hostname,
+    port: Number(dbUrl.port) || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
       },
-    })
-      
-  : new Sequelize({
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT), // Преобразуем в число
-      dialect: "postgres",
-    });
+    },
+  };
+}
 
-module.exports = sequelize;
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: "postgres",
+  },
+  production: productionConfig,
+};
